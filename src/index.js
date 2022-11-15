4eaa01fedc261a1b9398faa5b2bded45f6bbe416
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-15 21:03:04 +0000

    Add circle texture and style h2 title

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import gsyFlag from "./images/flag.png"
 import searchIcon from "./images/search.png"
 import logo from "./images/logo.png"
 import hero from "./images/herowithlight.png"
+import circleTexture from "./images/circle-vector.png"
 
 
 const gsyFlagImg = document.querySelector(".guernsey-flag")
 const searchIconImg = document.querySelector(".search-icon")
 const logoImg = document.querySelector(".logo")
 const heroSection = document.querySelector(".hero-section")
+const circleText = document.querySelector(".circle-texture")
 
 
 gsyFlagImg.src = gsyFlag
 logoImg.src = logo
 heroSection.style.backgroundImage = `url(${hero})`
+circleText.src = circleTexture
