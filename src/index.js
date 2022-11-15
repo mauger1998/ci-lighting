af8fa6ba71c722e1ac70a960c8d034333fe21cbd
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-15 15:36:23 +0000

    Import logo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import "./style.css"
 import gsyFlag from "./images/flag.png"
 import searchIcon from "./images/search.png"
+import logo from "./images/logo.png"
 
 const gsyFlagImg = document.querySelector(".guernsey-flag")
 const searchIconImg = document.querySelector(".search-icon")
+const logoImg = document.querySelector(".logo")
 
 gsyFlagImg.src = gsyFlag
 searchIconImg.src = searchIcon
+logoImg.src = logo
