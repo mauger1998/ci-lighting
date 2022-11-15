2653ec6ffa21864c27e972b99df0e9f92d036ae7
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-15 20:00:08 +0000

    Clear html and css plus src in index to start over

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import "./style.css"
 import gsyFlag from "./images/flag.png"
 import searchIcon from "./images/search.png"
 import logo from "./images/logo.png"
-import hero from "./images/hero.png"
+
 
 const gsyFlagImg = document.querySelector(".guernsey-flag")
 const searchIconImg = document.querySelector(".search-icon")
 const logoImg = document.querySelector(".logo")
-const heroImg = document.querySelector(".hero-image")
 
 
 
-heroImg.src = hero
+
