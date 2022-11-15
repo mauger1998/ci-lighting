ca9c3c19042baee4534a7d4c5ec397745fbc0da0
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-15 15:28:11 +0000

    Add flex in top bar to horizontally align and import imgs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./style.css"
+import gsyFlag from "./images/flag.png"
+import searchIcon from "./images/search.png"
+
+const gsyFlagImg = document.querySelector(".guernsey-flag")
+const searchIconImg = document.querySelector(".search-icon")
+
+gsyFlagImg.src = gsyFlag
+searchIconImg.src = searchIcon
