ee930be41b854f6368d6c9cd3c7a9051d5bdd67f
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-15 21:08:13 +0000

    Import 3 icons for cards

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import searchIcon from "./images/search.png"
 import logo from "./images/logo.png"
 import hero from "./images/herowithlight.png"
 import circleTexture from "./images/circle-vector.png"
-
+import firstIcon from "./images/icon1.png"
+import secondIcon from "./images/icon2.png"
+import thirdIcon from "./images/icon3.png"
 
 const gsyFlagImg = document.querySelector(".guernsey-flag")
 const searchIconImg = document.querySelector(".search-icon")
 const logoImg = document.querySelector(".logo")
 const heroSection = document.querySelector(".hero-section")
 const circleText = document.querySelector(".circle-texture")
+
 
 
 gsyFlagImg.src = gsyFlag
