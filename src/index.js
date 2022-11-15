182b39894091fda5d98df2ef030cd0feb77591ff
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-15 20:36:07 +0000

    Add hero image in first section

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import "./style.css"
 import gsyFlag from "./images/flag.png"
 import searchIcon from "./images/search.png"
 import logo from "./images/logo.png"
+import hero from "./images/herowithlight.png"
 
 
 const gsyFlagImg = document.querySelector(".guernsey-flag")
 const searchIconImg = document.querySelector(".search-icon")
 const logoImg = document.querySelector(".logo")
-
-
+const heroSection = document.querySelector(".hero-section")
 
 
 gsyFlagImg.src = gsyFlag
 logoImg.src = logo
+heroSection.style.backgroundImage = `url(${hero})`
