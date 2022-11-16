7a3274310bdf0602901d9c7f01d074d6ac01c0b5
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-16 13:04:16 +0000

    Make nav responsive add burger menu

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import circleTexture from "./images/circle-vector.png"
 import firstIcon from "./images/icon1.png"
 import secondIcon from "./images/icon2.png"
 import thirdIcon from "./images/icon3.png"
+import hamburger from "./images/Group 26.png"
 
 const gsyFlagImg = document.querySelector(".guernsey-flag")
 const searchIconImg = document.querySelector(".search-icon")
 const logoImg = document.querySelector(".logo")
 const heroSection = document.querySelector(".hero-section")
 const circleText = document.querySelector(".circle-texture")
+const menu = document.querySelector(".burger")
 
 
 
@@ -20,3 +22,4 @@ gsyFlagImg.src = gsyFlag
 logoImg.src = logo
 heroSection.style.backgroundImage = `url(${hero})`
 circleText.src = circleTexture
+menu.src = hamburger
