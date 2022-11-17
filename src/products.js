ae042069739cf286b7253171b179d2771481b99c
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-11-17 21:07:10 +0000

    Add hero image for product page

--- src/products.js
+++ src/products.js
@@ -1,9 +1,12 @@
 import "./style.css"
 import logo from "./images/logo.png"
 import flag from "./images/flag.png"
+import newHero from "./images/producthero.png"
 
 const logoImg = document.querySelector(".logo")
 const flagImg = document.querySelector(".guernsey-flag")
+const heroSec = document.querySelector(".hero-section-products")
 
 logoImg.src = logo
 flagImg.src = flag
+heroSec.style.backgroundImage = `url(${newHero})`
